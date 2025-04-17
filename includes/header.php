6a29c6c1d3a529feb91cb5fced75d6b6a830c6e2
Tushar26-web <tnarang@algomau.ca>
2025-04-17 15:44:59 -0400

    header

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<!-- includes/header.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Final Exam</title>
+    <link rel="icon" href="https://smartcdn.gprod.postmedia.digital/nexus/wp-content/uploads/2022/10/1004-bird.jpg" type="image/jpeg">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+    <h1>Final Exam Web App</h1>
+</header>
